3eee23aa7e3ee7bd5f9ee0173a469bb86ffb7ee7
Jean-Marie Poissonnier <jm.poissonnier@gmail.com>
2018-06-02 18:26:36 +0200

    Mongodb API change

--- src/database.js
+++ src/database.js
@@ -4,27 +4,29 @@ const MongoClient = require('mongodb').MongoClient
 const log = require('./log')
 const { connection_string } = require('./settings')
 
-let database
+let mongoClient
 
 const connect = () =>
     MongoClient.connect(connection_string)
-        .then(db => {
-            log.info(`Connected to database ${db.databaseName} successfully.`)
-            database = db
-            return db
+        .then(client => {
+            mongoClient = client
+            const database = client.db()
+            log.info(`Connected to database ${database.databaseName} successfully.`)
+            return database
         })
         .catch(err => {
             log.error('Unable to connect to database.')
             throw err
         })
 
-const getDb = () => database
+const getDb = () => mongoClient.db()
 
 const close = () => {
-    if (database) {
+    if (mongoClient) {
+        const database = mongoClient.db()
         log.info(`Closing database connection to ${database.databaseName}.`)
-        database.close()
-        database = null
+        mongoClient.close()
+        mongoClient = null
     }
 }
 
