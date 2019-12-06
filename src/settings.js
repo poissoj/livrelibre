c33e85b5ae1c557adea8d7f712a954c58e518ab6
Jean-Marie Poissonnier <jmp@jm-it.fr>
2019-12-06 20:32:31 +0100

    ✨ Read conf from .env file
    Env variables can now be set in .env instead of in settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const settings = {
     port: process.env.PORT || 8888,
     appName: process.env.APP_NAME || 'Livre Libre',
     cookieSecret: process.env.COOKIE_SECRET || '38cwimbiaJO9MDYMNZRX2jT',
     connection_string: process.env.MONGOLAB_URL || 'mongodb://127.0.0.1:27017/tsavant'
 };
+
 module.exports = settings;
