2d6979725973ab6ddab9dc38ad960e786823631c
Jean-Marie Poissonnier <jmp@jm-it.fr>
2020-05-03 17:41:56 +0200

    🐛 Rewrite deprecated group command with $group
    The group command was deprecated in MongoDB 3.6.
    Use the aggregation framework instead.

--- src/getCart.js
+++ src/getCart.js
@@ -3,22 +3,39 @@
 const database = require('./database');
 const log = require('./log');
 
-const reduce = function(item,res) {
-    res.total += item.price * 100;
-    res.count += item.quantity;
-};
-
-const getCart = username =>
+const getCart = (username) =>
     database
         .getDb()
         .collection('cart')
-        .group([], { username }, { total: 0, count: 0 }, reduce, true)
+        .aggregate([
+            { $match: { username } },
+            {
+                $group: {
+                    _id: null,
+                    total: {
+                        $sum: {
+                            $multiply: [
+                                {
+                                    $convert: {
+                                        input: '$price',
+                                        to: 'double',
+                                    },
+                                },
+                                '$quantity',
+                                100,
+                            ],
+                        },
+                    },
+                },
+            },
+        ])
+        .toArray()
         .then(results => {
-            const cart = results[0] || {total:0};
+            const cart = results[0] || { total: 0 };
             cart.total = (cart.total / 100).toFixed(2);
             return cart;
         })
-        .catch(err => {
+        .catch((err) => {
             log.error('getCart', err);
             return {};
         });
