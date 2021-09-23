a75b1128511479884156e1a2244315b78bc73ecb
Jean-Marie Poissonnier <jmp@jm-it.fr>
2021-09-23 12:48:35 +0200

    🚨 Fix TypeScript errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from "next/dist/shared/lib/router/router";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
