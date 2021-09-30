863475985dc9308af47ebba0b5e1b8b7903cbbed
Jean-Marie Poissonnier <jmp@jm-it.fr>
2021-09-30 11:51:33 +0200

    🔧 Update tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,29 @@
 module.exports = {
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: {
+        default: "hsl(174deg 100% 29%)",
+        dark: "hsl(174deg 100% 25%)",
+      },
+      gray: {
+        light: "hsl(220deg 13% 91%)",
+        default: "hsl(198.8deg 19% 16.5%)",
+        dark: "hsl(216.9deg 19.1% 26.7%)",
+        darker: "hsl(218deg 19% 11%)",
+        darkest: "hsl(197.1deg 21.2% 6.5%)",
+      },
+      white: "#FFF",
+    },
+    extend: {
+      spacing: {
+        xs: "4px",
+        sm: "8px",
+        md: "16px",
+        lg: "24px",
+      },
+    },
+  },
   plugins: [],
 };
