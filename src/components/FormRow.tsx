c242d9da82e4f2204999cee61d802ad2e420f0a8
Jean-Marie Poissonnier <jmp@jm-it.fr>
2021-10-07 12:45:19 +0200

    ✨ Add content for page "Advanced search"

---
+++ src/components/FormRow.tsx
@@ -0,0 +1,16 @@
+import tw from "twin.macro";
+
+const Label = tw.label`flex cursor-pointer mb-md align-items[center]`;
+const RowLabel = tw.span`text-right mr-lg font-medium w-1/4 flex-shrink-0 whitespace-nowrap`;
+const Wrapper = tw.div`flex w-2/3`;
+
+type FormRowProps = {
+  label: string;
+  children: React.ReactNode;
+};
+export const FormRow = ({ label, children }: FormRowProps): JSX.Element => (
+  <Label>
+    <RowLabel>{label}</RowLabel>
+    <Wrapper>{children}</Wrapper>
+  </Label>
+);
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,86 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Card } from "@/components/Card";
+import { Input, Select, Textarea } from "@/components/FormControls";
 import { Title } from "@/components/Title";
+import { FormRow } from "@/components/FormRow";
+import { Button } from "@/components/Button";
+import tw from "twin.macro";
+
+const Column = tw.div`flex-1 min-width[20rem]`;
 
 const Search = (): JSX.Element => {
   return (
-    <div>
+    <div tw="margin-left[10%] margin-right[10%] flex-1">
       <Title>Chercher un article</Title>
-      Chercher un article
+      <Card title="Chercher un article" tw="mb-lg">
+        <form>
+          <div tw="flex flex-wrap gap-lg">
+            <Column>
+              <FormRow label="Type">
+                <Select name="type" defaultValue="ignore">
+                  <option value="">--ignorer--</option>
+                  <option value="book">Livre</option>
+                  <option value="game">Jeu</option>
+                  <option value="postcard">Carte postale</option>
+                  <option value="stationery">Papeterie</option>
+                  <option value="dvd">DVD</option>
+                  <option value="unknown">Inconnu</option>
+                </Select>
+              </FormRow>
+              <FormRow label="ISBN">
+                <Input type="text" name="isbn" maxLength={13} />
+              </FormRow>
+              <FormRow label="Auteur">
+                <Input type="text" name="author" />
+              </FormRow>
+              <FormRow label="Titre">
+                <Input type="text" name="title" />
+              </FormRow>
+              <FormRow label="Éditeur">
+                <Input type="text" name="publisher" />
+              </FormRow>
+              <FormRow label="Distributeur">
+                <Input type="text" name="distributor" />
+              </FormRow>
+              <FormRow label="Mots-clés">
+                <Input type="text" name="keywords" />
+              </FormRow>
+            </Column>
+            <Column>
+              <FormRow label="Date d&rsquo;achat">
+                <Input type="text" name="datebought" />
+              </FormRow>
+              <FormRow label="Commentaires">
+                <Textarea name="comments" />
+              </FormRow>
+              <FormRow label="Prix d&rsquo;achat">
+                <Input type="number" name="purchasePrice" min={0} step={0.01} />
+              </FormRow>
+              <FormRow label="Prix de vente">
+                <Input type="number" name="price" min={0} step={0.01} />
+              </FormRow>
+              <FormRow label="Quantité">
+                <Input type="number" name="quantity" min={0} defaultValue={1} />
+              </FormRow>
+              <FormRow label="TVA">
+                <Select name="tva" defaultValue="5.5">
+                  <option value="20">20%</option>
+                  <option value="5.5">5.5%</option>
+                  <option value="2.1">2.1%</option>
+                  <option value="0">0%</option>
+                </Select>
+              </FormRow>
+            </Column>
+          </div>
+          <div tw="border-top[1px solid #ddd] mt-sm flex justify-content[flex-end] padding[16px 8px 8px 0]">
+            <Button tw="px-md">
+              <FontAwesomeIcon icon={faSearch} tw="mr-sm" />
+              Rechercher
+            </Button>
+          </div>
+        </form>
+      </Card>
     </div>
   );
 };
