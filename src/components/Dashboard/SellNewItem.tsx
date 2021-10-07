10b0b3e661c024fea9475839e55af6e966436d33
Jean-Marie Poissonnier <jmp@jm-it.fr>
2021-10-07 14:53:24 +0200

    ♻️  Use <FormRow> in SellNewItem

--- src/components/Dashboard/SellNewItem.tsx
+++ src/components/Dashboard/SellNewItem.tsx
@@ -3,24 +3,19 @@ import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "@/components/Button";
 import { Input, Select } from "@/components/FormControls";
-import tw from "twin.macro";
-
-const Label = tw.label`flex cursor-pointer mb-md align-items[center]`;
-const RowLabel = tw.span`text-right mr-lg font-medium w-1/4`;
+import { FormRow } from "@/components/FormRow";
+import "twin.macro";
 
 export const SellNewItem = (): JSX.Element => (
   <Card title="Vendre un article non répertorié">
-    <form tw="flex flex-col pr-lg">
-      <Label>
-        <RowLabel>Prix</RowLabel>
+    <form tw="flex flex-col">
+      <FormRow label="Prix">
         <Input type="text" name="price" />
-      </Label>
-      <Label>
-        <RowLabel>Titre</RowLabel>
+      </FormRow>
+      <FormRow label="Titre">
         <Input type="text" name="title" placeholder="Article indépendant" />
-      </Label>
-      <Label>
-        <RowLabel>Type</RowLabel>
+      </FormRow>
+      <FormRow label="Type">
         <Select name="type" defaultValue="book">
           <option value="unknown">Inconnu</option>
           <option value="book">Livre</option>
@@ -29,16 +24,15 @@ export const SellNewItem = (): JSX.Element => (
           <option value="stationery">Papeterie</option>
           <option value="dvd">DVD</option>
         </Select>
-      </Label>
-      <Label>
-        <RowLabel>TVA</RowLabel>
+      </FormRow>
+      <FormRow label="TVA">
         <Select name="tva" defaultValue="5.5">
           <option value="20">20%</option>
           <option value="5.5">5.5%</option>
           <option value="2.1">2.1%</option>
           <option value="0">0%</option>
         </Select>
-      </Label>
+      </FormRow>
       <Button disabled tw="align-self[center]">
         <FontAwesomeIcon icon={faCartPlus} tw="mr-sm" />
         Ajouter au panier
