0f25b71bf6eba28f4e55d538a33af91fbea84f48
Jean-Marie Poissonnier <jmp@jm-it.fr>
2022-02-14 19:53:08 +0100

    ♻️  Cache MongoDB client instance
    Merge both MONGODB env vars into one

--- src/server/database.ts
+++ src/server/database.ts
@@ -1,17 +1,32 @@
 import { Db, MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URL) {
-  throw new Error("MONGOBD_URL is not defined");
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGOBD_URI is not defined");
 }
 
-const DB_NAME = process.env.MONGODB_DB_NAME;
-if (!DB_NAME) {
-  throw new Error("MONGOBD_DB_NAME is not defined");
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-const client = new MongoClient(process.env.MONGODB_URL);
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(process.env.MONGODB_URI);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(process.env.MONGODB_URI);
+  clientPromise = client.connect();
+}
 
 export const getDb = async (): Promise<Db> => {
-  await client.connect();
-  return client.db(DB_NAME);
+  const mongoClient = await clientPromise;
+  return mongoClient.db();
 };
