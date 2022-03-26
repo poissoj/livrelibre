9bd56e1fb457a8a48226b840efb7748f5dae78e4
Jean-Marie Poissonnier <jmp@jm-it.fr>
2022-03-26 12:59:29 +0100

    💄 Add custom error 500 page

---
+++ src/pages/500.tsx
@@ -0,0 +1,10 @@
+import "twin.macro";
+
+const Custom500 = (): JSX.Element => (
+  <div tw="flex-1 self-center text-center text-2xl">
+    <h1 tw="font-bold">Erreur 500</h1>
+    <span>Erreur interne</span>
+  </div>
+);
+
+export default Custom500;
