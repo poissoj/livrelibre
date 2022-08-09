b2f84144cc07ae957d3d708c333ac6943e27d9d8
Jean-Marie Poissonnier <jmp@jm-it.fr>
2022-08-09 12:05:02 +0200

    🔥 Delete v1 files

--- src/log.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const log = level => (...args) => console[level](new Date().toISOString(), ...args);
-
-module.exports = {
-    info: log('info'),
-    warn: log('warn'),
-    error: log('error')
-};
