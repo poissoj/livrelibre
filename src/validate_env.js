ecb62b42cdc15d1803ec420d99d3e756c649a0f0
Jean-Marie Poissonnier <jmp@jm-it.fr>
2024-04-12 16:05:32 +0200

    🔧 MongoDB: enable SRV Connection format

--- src/validate_env.js
+++ src/validate_env.js
@@ -6,7 +6,10 @@ const envSchema = z.object({
   COOKIE_PASSWORD: z.string().min(32),
   ISBN_SEARCH_URL: z.string().url().optional(),
   LOG_LEVEL: z.string().optional(),
-  MONGODB_URI: z.string().startsWith("mongodb://"),
+  MONGODB_URI: z
+    .string()
+    .startsWith("mongodb://")
+    .or(z.string().startsWith("mongodb+srv://")),
   NEXT_PUBLIC_APP_NAME: z.string().optional(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   AUTHORIZED_DOMAINS: z.string().optional(),
