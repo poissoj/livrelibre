dd2338f6c457af1d7a0885a16c1c1c9fcd02c28b
Jean-Marie Poissonnier <jmp@jm-it.fr>
2024-05-24 18:30:48 +0200

    ✨ Orders table: move Date column to the left

--- src/components/OrdersTable.tsx
+++ src/components/OrdersTable.tsx
@@ -12,11 +12,11 @@ export const OrdersTable = ({ items }: { items: Order[] }) => {
     <table className="flex-1">
       <thead>
         <tr className="sticky top-0 bg-white z-10">
+          <th className="text-left">Date</th>
           <th className="text-left">Nom</th>
           <th className="text-left">Titre</th>
           <th className="text-left">ISBN</th>
           <th className="text-left">Distributeur</th>
-          <th className="text-left">Date</th>
           <th className="text-left">État</th>
           <th className="text-left">Client prévenu</th>
           <th className="text-left">Payé</th>
@@ -30,11 +30,11 @@ export const OrdersTable = ({ items }: { items: Order[] }) => {
             className="cursor-pointer hover:bg-gray-light"
             onClick={() => router.push(`/order/${item._id}`)}
           >
+            <td>{formatDateFR(item.date)}</td>
             <td>{item.customer.fullname}</td>
             <td>{item.itemTitle}</td>
             <td>{item.item?.isbn}</td>
             <td>{item.item?.distributor}</td>
-            <td>{formatDateFR(item.date)}</td>
             <td>{STATUS_LABEL[item.ordered]}</td>
             <td>{formatBool(item.customerNotified)}</td>
             <td>{formatBool(item.paid)}</td>
