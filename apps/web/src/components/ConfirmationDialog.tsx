906b6c4e6184ec622d29a5ad20c33b34857cfbde
Jean-Marie Poissonnier <jmp@jm-it.fr>
2026-09-15 22:55:54 +0200

    💫 Fix confirmation dialog width and animation

--- apps/web/src/components/ConfirmationDialog.tsx
+++ apps/web/src/components/ConfirmationDialog.tsx
@@ -36,37 +36,48 @@ export const ConfirmationDialog = (props: {
         appear
         show={isOpen}
         enter="ease-out duration-300"
-        enterFrom="opacity-0 transform-[scale(95%)]"
-        enterTo="opacity-100 transform-[scale(100%)]"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
         leave="ease-in duration-200"
-        leaveFrom="opacity-100 transform-[scale(100%)]"
-        leaveTo="opacity-0 transform-[scale(95%)]"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
       >
         <Dialog onClose={close} className="relative">
           <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
           <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-            <DialogPanel className="max-w-lg space-y-4 bg-white p-8 rounded-xl">
-              <DialogTitle className="font-bold">{props.title}</DialogTitle>
-              <p>{props.message}</p>
-              <div className="flex">
-                <Button
-                  type="button"
-                  className="!bg-[#991b1b]"
-                  onClick={props.onConfirm}
-                >
-                  <FontAwesomeIcon icon={faTrash} className="mr-sm" />
-                  Oui, supprimer
-                </Button>
-                <Button
-                  type="button"
-                  className="ml-auto !bg-[#6E6E6E]"
-                  onClick={close}
-                >
-                  <FontAwesomeIcon icon={faTimesCircle} className="mr-sm" />
-                  Non, annuler
-                </Button>
-              </div>
-            </DialogPanel>
+            <Transition
+              appear
+              show={isOpen}
+              enter="ease-out duration-300"
+              enterFrom="scale-95"
+              enterTo="scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="scale-100"
+              leaveTo="scale-95"
+            >
+              <DialogPanel className="w-full max-w-lg space-y-4 bg-white p-8 rounded-xl">
+                <DialogTitle className="font-bold">{props.title}</DialogTitle>
+                <p>{props.message}</p>
+                <div className="flex">
+                  <Button
+                    type="button"
+                    className="!bg-[#991b1b]"
+                    onClick={props.onConfirm}
+                  >
+                    <FontAwesomeIcon icon={faTrash} className="mr-sm" />
+                    Oui, supprimer
+                  </Button>
+                  <Button
+                    type="button"
+                    className="ml-auto !bg-[#6E6E6E]"
+                    onClick={close}
+                  >
+                    <FontAwesomeIcon icon={faTimesCircle} className="mr-sm" />
+                    Non, annuler
+                  </Button>
+                </div>
+              </DialogPanel>
+            </Transition>
           </div>
         </Dialog>
       </Transition>
